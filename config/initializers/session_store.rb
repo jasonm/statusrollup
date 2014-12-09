cf100ec0a9ea6c3b159fc54792642fe53ff29721
Jason Morrison <jmorrison@minervaproject.com>
2014-12-08 18:32:39 -0800

    Deprecate status-rollup in favor of GitHub feature

--- config/initializers/session_store.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-StatusRollup::Application.config.session_store :cookie_store, key: '_status_rollup_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# StatusRollup::Application.config.session_store :active_record_store
--- spec/factories/repo.rb
+++
@@ -1,7 +0,0 @@
-FactoryGirl.define do
-  factory :repo do
-    association :user
-    sequence(:repo_name) { |n| "repo_name#{n}" }
-    user_name { user.nickname }
-  end
-end
